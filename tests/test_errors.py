b6ea061e5e1000871b7c5011856d4029dec3947b
Evan Baird <evanbaird@evans-mbp.lan>
2024-03-09 15:12:55 -0700

    Adding tests with notes

---
+++ tests/test_errors.py
@@ -0,0 +1,8 @@
+'''
+In these tests we'll make sure a GraphQL error is raised as well as checking the message
+of those raised exceptions as well. 
+
+Scan all types and make a test for the errors! Remember to use `with pytest.raises(graphql.GraphQLError)`
+
+Check Pytest chapter 2 Testing for Expected Exceptions
+'''
