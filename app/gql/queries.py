861c2612411bec5de0b4b0deed76e90f0d6c1927
Evan Baird <evanbaird@evans-mbp.lan>
2024-03-09 12:22:16 -0700

    moving to render

--- app/gql/queries.py
+++ app/gql/queries.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from graphene import Field, Int, List, ObjectType
+from graphql import GraphQLError
 from sqlalchemy import select
 
 from app.db.database import Session
@@ -8,18 +9,24 @@ from app.gql.types import StiglerType
 
 
 class Query(ObjectType):
-    foods = List(StiglerType, limit=Int())
+    foods = List(StiglerType, limit=Int(), skip=Int())
     food = Field(StiglerType, idx=Int(required=True))
 
     @staticmethod
-    def resolve_foods(root, info, limit=None):
+    def resolve_foods(root, info, limit=None, skip=None):
         with Session() as session:
-            if limit is not None:
-                return session.scalars(select(Stigler).limit(limit)).all()
+            if limit is None and skip is not None:
+                return GraphQLError("Limit must be specified with skip.")
+
+            if limit:
+                return session.scalars(
+                    select(Stigler).limit(limit).offset(skip),
+                ).all()
+
             else:
                 return session.scalars(select(Stigler)).all()
 
     @staticmethod
-    def resolve_food(root, info, idx):
+    def resolve_food(root, info, id):
         with Session() as session:
-            return session.scalar(select(Stigler).where(Stigler.idx == idx))
+            return session.scalar(select(Stigler).where(Stigler.idx == id))
